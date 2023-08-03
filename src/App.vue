42f68001367673e7762f8b26e6815204dc0f5ba0
Christian Moya <cmoyacharro@gamil.com>
2023-08-03 09:39:30 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <Estudiante />
+  <br>
+  <br>
+  <br>
+  <GuardaEstudiante />
+  <ActualizarEstudiante/>
+  <EliminarEstudiante/>
+</template>
+
+<script>
+import Estudiante from './modules/estudiante/pages/ConsultaEstudiante.vue'
+import GuardaEstudiante from './modules/estudiante/pages/GuardaEstudiante.vue'
+import ActualizarEstudiante from './modules/estudiante/pages/ActualizarEstudiante.vue';
+import EliminarEstudiante from './modules/estudiante/pages/EliminarEstudiante.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Estudiante,
+    GuardaEstudiante,
+    ActualizarEstudiante,
+    EliminarEstudiante
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/modules/estudiante/pages/ConsultaEstudiante.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>CONSULTAR ESTUDIANTE</h1>
+  <DatosEstudiante/>
+</template>
+
+<script>
+import DatosEstudiante from '../components/DatosEstudiante.vue'
+
+export default {
+  components: {
+    DatosEstudiante
+  }
+}
+</script>
+
+<style>
+
+</style>
